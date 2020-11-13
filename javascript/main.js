d2e99a68c891e64e50c994662149bbc70da774b5
LEE MIRU <50521697+leemiru@users.noreply.github.com>
2020-11-13 18:40:48 +0900

    201113
    - 각종 레이아웃 위치 안정화
    - 프로젝트 갤러리 수정
    - Pagination 수정

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,26 @@
-function moveTop(){
-     $('html, body').animate({scrollTop : 0}, 500);
-}	
+// Real cursor element
+
+const root = document.querySelector('html')
+
+const cursor = document.createElement('div')
+cursor.classList.add('cursor')
+root.appendChild(cursor)
+
+var timer;
+
+
+root.addEventListener('mousemove', (e) => {
+	cursor.classList.add('moving')
+	setPosition(cursor, e)
+	clearTimeout(timer);
+    timer=setTimeout(mouseStopped, 1500);   
+})
+
+function mouseStopped() {
+	cursor.classList.remove('moving');
+}
+
+function setPosition(element, e) {
+	element.style.transform = `translate3d(${e.clientX}px, ${e.clientY}px, 0)`
+}
+
