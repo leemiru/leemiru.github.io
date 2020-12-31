04e5f7ed5033a870a642c0fc50cb0695a4dd7d97
LEE MIRU <leemiru@khu.ac.kr>
2020-12-31 20:45:58 +0900

    Cursor Disabled
    so sad

--- javascript/main.js
+++ javascript/main.js
@@ -1,27 +1,27 @@
 // Real cursor element
 
-const root = document.querySelector('html')
+// const root = document.querySelector('html')
 
-const cursor = document.createElement('div')
-cursor.classList.add('cursor')
-root.appendChild(cursor)
+// const cursor = document.createElement('div')
+// cursor.classList.add('cursor')
+// root.appendChild(cursor)
 
-var timer;
+// var timer;
 
-root.addEventListener('mousemove', (e) => {
-	cursor.classList.add('moving')
-	setPosition(cursor, e)
-	clearTimeout(timer);
-    timer=setTimeout(mouseStopped, 1500);   
-})
+// root.addEventListener('mousemove', (e) => {
+// 	cursor.classList.add('moving')
+// 	setPosition(cursor, e)
+// 	clearTimeout(timer);
+//     timer=setTimeout(mouseStopped, 1500);   
+// })
 
-function mouseStopped() {
-	cursor.classList.remove('moving');
-}
+// function mouseStopped() {
+// 	cursor.classList.remove('moving');
+// }
 
-function setPosition(element, e) {
-	element.style.transform = `translate3d(${e.clientX}px, ${e.clientY}px, 0)`
-}
+// function setPosition(element, e) {
+// 	element.style.transform = `translate3d(${e.clientX}px, ${e.clientY}px, 0)`
+// }
 
 
 function openPost(url) {
