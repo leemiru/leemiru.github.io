68e4d0446e72ab15c3751dfc0e5c9e2bd1710b6c
LEE MIRU <leemiru@khu.ac.kr>
2021-01-02 16:44:54 +0900

    close btn, gradient bg
    g

--- javascript/main.js
+++ javascript/main.js
@@ -26,20 +26,35 @@
 
 function openPost(url) {
 	$('#postframe').attr('src', '/images/posts/'+url+'/post.html');
-	$('.cursor').fadeOut();
-	setTimeout(function() {
-		$('#postModal').css('animation-name', 'postModalopen');
-		$('#postModal').css('top', '0%');
-	},200);
+
+	$('#postModal').css('animation-name', 'postModalopen');
+	$('#postModal').css('top', '0%');
+
 	$('#block-main').css('animation-name', 'postModalopenbg');
 	$('#block-main').css('opacity', '0');
+
+	$('#gallery-pagination').css('display', 'none');
+	
+	$('#btn-close').css('display', 'inline-block');
+
+	console.log(url + ' post opened');
   }
 
 function closePost() {
-	$('.cursor').fadeIn();
 	$('#postModal').css('animation-name', 'postModalclose');
 	$('#postModal').css('top', '100%');
+
 	$('#block-main').css('animation-name', 'postModalclosebg');
 	$('#block-main').css('opacity', '1');
+
+	$('#gallery-pagination').css('display', 'flex');
+
+	$('#btn-close').css('display', 'none');
+
+	setTimeout(function(){
+		$('#postframe').attr('src', ' ');
+	},200);
+
+	console.log('post closed');
   }
 
