8baeb0e67a732d37849e5f61c1c3bff3a1debda4
LEE MIRU <leemiru@khu.ac.kr>
2021-01-04 17:16:37 +0900

    Layout Density, Clean Code

--- javascript/main.js
+++ javascript/main.js
@@ -1,40 +1,22 @@
-// Real cursor element
+var $close;
+var $pagination;
+var $header;
+var $footer;
+var $intro;
+var $main;
+var $postframe;
+var $body;
 
-// const root = document.querySelector('html')
-
-// const cursor = document.createElement('div')
-// cursor.classList.add('cursor')
-// root.appendChild(cursor)
-
-// var timer;
-
-// root.addEventListener('mousemove', (e) => {
-// 	cursor.classList.add('moving')
-// 	setPosition(cursor, e)
-// 	clearTimeout(timer);
-//     timer=setTimeout(mouseStopped, 1500);
-// })
-
-// function mouseStopped() {
-// 	cursor.classList.remove('moving');
-// }
-
-// function setPosition(element, e) {
-// 	element.style.transform = `translate3d(${e.clientX}px, ${e.clientY}px, 0)`
-// }
 function openPost(url) {
   $("#postframe").attr("onload", "upPost(this)");
   $("#postframe").attr("src", "/images/posts/" + url + "/post.html");
 }
 
 function upPost() {
-  $(".body").css("overflow", "hidden");
+  $("body").addClass("lock-position");
+
   $("#postModal").css("animation-name", "postModalopen");
   $("#postModal").css("top", "0%");
-
-  $('.header').css('background-color', 'rgba(0, 0, 0, 0');
-  $('.footer').css('background-color', 'rgba(0, 0, 0, 0');
-
   $("#block-main").css("animation-name", "postModalopenbg");
   $("#block-main").css("opacity", "0");
 
@@ -50,22 +32,22 @@ function closePost() {
   $("#postModal").css("animation-name", "postModalclose");
   $("#postModal").css("top", "100%");
 
-  $('.header').css('background-color', 'rgba(0, 0, 0, 0.9');
-  $('.footer').css('background-color', 'rgba(0, 0, 0, 0.9');
-
   $("#block-main").css("animation-name", "postModalclosebg");
   $("#block-main").css("opacity", "1");
 
   $("#gallery-pagination").css("display", "flex");
 
   $("#btn-close").css("display", "none");
+
   $("#btn-intro").css("display", "inline-block");
 
-  // setTimeout(function(){
-  // 	$('#postframe').attr('src', ' ');
-  // },200);
-  $(".body").css("overflow", "auto");
+  $("body").removeClass("lock-position");
   console.log("post closed");
+}
+
+function onBlur() {
+  $(".header").toggleClass("blur");
+  $(".footer").toggleClass("blur");
 }
 
 $(window).scroll(function () {
@@ -76,7 +58,7 @@ $(window).scroll(function () {
 
   if (deg < 0) {
     deg == 0;
-  } else if (deg > 360) {
+  } else if (deg >= 360) {
     deg == 360;
   }
 
@@ -84,27 +66,34 @@ $(window).scroll(function () {
     deg == 0;
   }
 
+  //gallery Parallax;
   $("#swiper-gallery").css("transform", "translateY(" + $height / 1.75 + "px)");
   $("#swiper-gallery").css("opacity", 100 - $height / 10 + "%");
 
   //header는 스크롤을 바닥에 닿자마자 반전
   if ($height > $body - 30) {
     $(".body").css("background-color", "#fff");
-    $(".header").css("filter", "invert(1)");
-    $("#gallery-pagination").css("display", "none");
+    $(".header").css("filter", "invert(1) hue-rotate(180deg)");
+	$("#gallery-pagination").css("display", "none");
+	$(".header").addClass('blur');
   } else {
     $(".body").css("background-color", "#000");
     $(".header").css("filter", "invert(0)");
     $("#gallery-pagination").css("display", "flex");
-    $("#btn-intro").text("⍗");
+	$("#btn-intro").text("⍗");
+	$(".header").removeClass('blur');
   }
 
-  ////footer는 스크롤을 시작하자마자 반전
+  //footer는 스크롤을 시작하자마자 반전
   if ($height > 60) {
-    $(".footer").css("filter", "invert(1)");
-    $("#btn-intro").text("⍐");
+	$(".footer").css("filter", "invert(1)  hue-rotate(180deg)");
+	$(".footer").addClass('blur');
+	$("#btn-intro").text("⍐");
+	stopBounce();
+	
   } else {
-    $(".footer").css("filter", "invert(0)");
+	$(".footer").css("filter", "invert(0)");
+	$(".footer").removeClass('blur');
   }
 
   $("#main-logo-image").css("transform", "rotateX(" + deg + "deg)");
@@ -115,13 +104,16 @@ function gotoIntro() {
   var $body = $("#main-contents").height();
 
   if ($height < $body) {
-    $("#btn-intro").text("⍐");
+
     $("html, body").animate(
       { scrollTop: $("#intro-contents").offset().top },
       500
     );
   } else if ($height >= $body) {
-    $("#btn-intro").text("⍗");
     $("html, body").animate({ scrollTop: 0 }, 500);
   }
+}
+
+function stopBounce() {
+	$("#btn-intro").css("animation-iteration-count", "1");
 }
